6789d53c6b499f5db86684507203d2d2803267a8
bathalapavankumar1 <bathalapavankumar1@gmail.com>
2025-04-21 23:41:06 +0530

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\pavan final btech project\A_Road_Accident_Prediction\a_road_accident_prediction\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
